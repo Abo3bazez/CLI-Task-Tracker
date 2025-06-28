43b81d6bd61f9e56a2fa025ca6198afc137d45ed
Ahmed Aziz <azez55.ali@gmail.com>
2025-06-28 16:35:49 +0300

    Add utility functions and structure for task management in CLI

---
+++ utility/Functions.js
@@ -0,0 +1,97 @@
+import fs from "fs";
+import chalk from "chalk";
+import { scriptStructure } from "./Structure.js";
+import { formatISO } from "date-fns";
+
+const log = console.log;
+
+let date = formatISO(new Date(), {
+  representation: "date",
+});
+
+async function checkFile() {
+  try {
+    await fs.access(scriptStructure.filePath, fs.constants.F_OK, (err) => {
+      if (err) {
+        // if file does not exist create it
+        fs.writeFile(scriptStructure.filePath, "", (err) => {
+          if (err) throw err;
+          console.log("Tasks File created successfully.");
+        });
+      } else {
+        console.log("Tasks File already exists. No action taken.");
+      }
+    });
+  } catch (err) {
+    log(err);
+  }
+}
+
+function taskStatus() {
+  log(chalk.bold("Total Tasks:", scriptStructure.tasks["All Tasks"].length));
+  log(
+    chalk.bold(
+      "Finished Tasks:",
+      scriptStructure.tasks["Finished Tasks"].length
+    )
+  );
+  log(
+    chalk.bold(
+      "InProgress Tasks:",
+      scriptStructure.tasks["inProgress Tasks"].length
+    )
+  );
+  log(
+    chalk.bold(
+      "UnFinished Tasks:",
+      scriptStructure.tasks["UnFinished Tasks"].length
+    )
+  );
+}
+
+function homeOptions() {
+  let i = 1;
+  Object.entries(scriptStructure.options).forEach(([key]) => {
+    log(chalk.bold(`${i} - ${key}`));
+    i++;
+  });
+}
+
+function logOptions(num) {
+  let i = 1;
+  if (num === "1") {
+    Object.entries(scriptStructure.options["Task Options"]).forEach(
+      ([key, value]) => {
+        log(chalk.bold(`${key} - ${value}`));
+        i++;
+      }
+    );
+  }
+  if (num === "2") {
+    Object.entries(scriptStructure.options["Edit Options"]).forEach(
+      ([key, value]) => {
+        log(chalk.bold(`${key} - ${value}`));
+        i++;
+      }
+    );
+  }
+}
+
+function backToOptions(num) {
+  if (num === "0") {
+    logOptions;
+  }
+}
+
+function displayTime() {
+  log(chalk.bold("Task Tracker CLI App", "|||", "Time:", date));
+}
+
+export {
+  checkFile,
+  taskStatus,
+  homeOptions,
+  backToOptions,
+  logOptions,
+  displayTime,
+};
---
+++ utility/Structure.js
@@ -0,0 +1,23 @@
+export let scriptStructure = {
+  filePath: "tasks.json",
+  options: {
+    "Task Options": {
+      1: "See All Tasks",
+      2: "See Finished Tasks",
+      3: "See UnFinished Tasks",
+      4: "See InProgress Tasks",
+    },
+    "Edit Options": {
+      1: "Add Task",
+      2: "Edit Task",
+      3: "Set Task To Finished",
+      4: "Set Task To InProgress",
+    },
+  },
+  tasks: {
+    "All Tasks": [],
+    "Finished Tasks": [],
+    "UnFinished Tasks": [],
+    "inProgress Tasks": [],
+  },
+};
